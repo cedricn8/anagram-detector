4516038f25df305e71d4ab5fb9bfbfc581c58069
cedricn8 <cedcn-9mail@yahoo.com>
2022-09-08 21:17:06 -0700

    commit tree d5a478c72ce2e2b09176332d050af38631d9e8ec

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+
+    #when new instance created, auto stores word in var @word
+    def initialize(word)
+        @word = word
+    end
+
+    #attr_accessor :word
+
+    def match(list_array)
+        matching_words = []
+        #convert word letters to array, sort the array, then convert back to string
+        word_sorted = @word.split("").sort.join
+        list_array.each do |item|
+            if word_sorted == item.split("").sort.join
+                matching_words << item
+            end
+        end
+        matching_words
+    end
+
+end
+
+
